49083e5a7693284e848276ab9d3b5455970bbcb8
PARIVENTHESWARAN PANEERSELVAM <parivenmc06@gmail.com>
2026-05-23 00:43:13 +0800

    Set explicit Next build paths

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: '.next',
+  experimental: {
+    outputFileTracingRoot: process.cwd(),
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
